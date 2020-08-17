9e5a0e2238f857f50a807a26b22773f1fe27e9f0
CodeTemplar99 <udoagwac@gmail.com>
2020-08-17 15:11:52 +0100

    list box

--- js/spine.js
+++ js/spine.js
@@ -6,3 +6,26 @@ function showSideBar() {
     isOpen = !isOpen
 
 }
+
+new Vue({
+    el: '#app',
+    data: {
+        formMessage: "Welcome,",
+        formDetail: "fill the form to sign up",
+        show_sign_up_form: true,
+        show_login_form: false,
+        show_sign_up_button: true,
+        show_log_in_button: false
+    },
+    methods: {
+        switch_to_login: function () {
+            if (this.show_sign_up_form = !true) {
+                this.formMessage = "Hi,"
+                this.formDetail = "login to order today"
+                this.show_sign_up_form = "!true"
+                this.show_sign_up_button = "!true"
+                this.show_login_button = "true"
+            }
+        }
+    }
+})
