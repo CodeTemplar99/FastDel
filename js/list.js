aca4a1bfab0c6229600444f186b440b3ea829b26
CodeTemplar99 <udoagwac@gmail.com>
2020-08-24 16:04:27 +0100

    account view

--- js/list.js
+++ js/list.js
@@ -1,5 +1,25 @@
 new Vue({
     el: '#list',
-    data: {},
-    methods: {}
+    data: {
+        show_list_view: true,
+        show_action_view: false,
+        show_account_view: false,
+    },
+    methods: {
+        display_list_view: function () {
+            this.show_list_view = true
+            this.show_action_view = false
+            this.show_account_view = false
+        },
+        display_action_view: function () {
+            this.show_list_view = false
+            this.show_action_view = true
+            this.show_account_view = false
+        },
+        display_account_view: function () {
+            this.show_list_view = false
+            this.show_action_view = false
+            this.show_account_view = true
+        }
+    }
 })
